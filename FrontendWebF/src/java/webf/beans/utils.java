5faa16e3342d7f245e7badf2a2aecb8efa434ec0
Andreas Moik <moik@technikum-wien.at>
2015-12-01 19:10:10 +0100

    kalender konverter

--- FrontendWebF/src/java/webf/beans/utils.java
+++ FrontendWebF/src/java/webf/beans/utils.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package webf.beans;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.xml.bind.DatatypeConverter;
 /**
  *
  * @author anti88
@@ -14,5 +12,23 @@ public class utils {
     static public void tinf(String msg)
     {
         System.out.println(msg);
+    }
+    
+
+
+    public static Date unmarshal(String dateTime) {
+        return DatatypeConverter.parseDate(dateTime).getTime();
+    }
+
+    public static String marshalDate(Date date) {
+        final GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        return DatatypeConverter.printDate(calendar);
+    }
+
+    public static String marshalDateTime(Date dateTime) {
+        final GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(dateTime);
+        return DatatypeConverter.printDateTime(calendar);
     }
 }
